338347fd30efdd67b15e88483437a73b598170d6
Diego <odiegodev@gmail.com>
2024-07-06 23:45:47 -0300

    feat: start using OOP pattern

--- Dagoba.js
+++ Dagoba.js
@@ -1,32 +1,51 @@
-V = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
-E = [
-  [1, 2],
-  [1, 3],
-  [2, 4],
-  [2, 5],
-  [3, 6],
-  [3, 7],
-  [4, 8],
-  [4, 9],
-  [5, 10],
-  [5, 11],
-  [6, 12],
-  [6, 13],
-  [7, 14],
-  [7, 15],
-];
+// V = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+// E = [
+//   [1, 2],
+//   [1, 3],
+//   [2, 4],
+//   [2, 5],
+//   [3, 6],
+//   [3, 7],
+//   [4, 8],
+//   [4, 9],
+//   [5, 10],
+//   [5, 11],
+//   [6, 12],
+//   [6, 13],
+//   [7, 14],
+//   [7, 15],
+// ];
 
-parents = (vertices) =>
-  E.reduce(
-    (acc, [parent, child]) =>
-      vertices.includes(child) ? acc.concat(parent) : acc,
-    [],
-  );
-children = (vertices) =>
-  E.reduce(
-    (acc, [parent, child]) =>
-      vertices.includes(parent) ? acc.concat(child) : acc,
-    [],
-  );
+// parents = (vertices) =>
+//   E.reduce(
+//     (acc, [parent, child]) =>
+//       vertices.includes(child) ? acc.concat(parent) : acc,
+//     [],
+//   );
+// children = (vertices) =>
+//   E.reduce(
+//     (acc, [parent, child]) =>
+//       vertices.includes(parent) ? acc.concat(child) : acc,
+//     [],
+//   );
 
-console.log(children(children(children(parents(parents(parents([8])))))));
+// console.log(children(children(children(parents(parents(parents([8])))))));
+
+//BUILD A BETTER GRAPH
+
+Dagoba.G = {};
+
+Dagoba.graph = function (V, E) {
+  const graph = Object.create(Dagoba.G);
+
+  graph.edges = [];
+  graph.vertices = [];
+  graph.vertexIndex = {};
+
+  graph.autoid = 1;
+
+  if (Array.isArray(V)) graph.addVertices(V);
+  if (Array.isArray(E)) graph.addEdges(E);
+
+  return graph;
+};
